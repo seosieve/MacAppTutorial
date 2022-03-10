5b5dfba2bae2467783d199c1305a419c871ab1df
seosieve <royalcircle97@naver.com>
2022-03-10 23:35:24 +0900

    Initial Commit

---
+++ MacAppTutorial/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MacAppTutorial
+//
+//  Created by 서충원 on 2022/03/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MacAppTutorial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacAppTutorial
+//
+//  Created by 서충원 on 2022/03/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
